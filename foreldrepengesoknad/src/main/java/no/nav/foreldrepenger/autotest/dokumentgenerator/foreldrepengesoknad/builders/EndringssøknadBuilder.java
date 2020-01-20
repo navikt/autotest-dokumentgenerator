1e86c2c0d44fa4d27a25b4dbd19cd58af69c141f
Christoffer <45653861+christofferlohne@users.noreply.github.com>
2020-01-20 15:15:06 +0100

    Sletter gammel IM og fikser på feil ved RefusjonBuilder (#11)

---
+++ foreldrepengesoknad/src/main/java/no/nav/foreldrepenger/autotest/dokumentgenerator/foreldrepengesoknad/builders/EndringssøknadBuilder.java
@@ -0,0 +1,43 @@
+package no.nav.foreldrepenger.autotest.dokumentgenerator.foreldrepengesoknad.builders;
+
+import no.nav.foreldrepenger.autotest.dokumentgenerator.foreldrepengesoknad.SøkersRolle;
+import no.nav.vedtak.felles.xml.soeknad.endringssoeknad.v3.Endringssoeknad;
+import no.nav.vedtak.felles.xml.soeknad.endringssoeknad.v3.ObjectFactory;
+import no.nav.vedtak.felles.xml.soeknad.felles.v3.Ytelse;
+import no.nav.vedtak.felles.xml.soeknad.uttak.v3.Fordeling;
+import no.nav.vedtak.felles.xml.soeknad.v3.Soeknad;
+
+public class EndringssøknadBuilder extends SøknadBuilder<EndringssøknadBuilder>{
+    private Endringssoeknad endringssoeknadKladd;
+
+    public EndringssøknadBuilder(String aktoerId, SøkersRolle søkerRolle) {
+        endringssoeknadKladd = new Endringssoeknad();
+        medSøker(aktoerId, søkerRolle);
+    }
+    @Override
+    protected EndringssøknadBuilder medYtelse(Ytelse ytelse) {
+        søknadKladd.setOmYtelse(setOmYtelseJAXBElement(
+                (new ObjectFactory()).createEndringssoeknad((Endringssoeknad) ytelse)));
+        return this;
+
+    }
+    @Override
+    protected EndringssøknadBuilder self() {
+        return this;
+    }
+    public EndringssøknadBuilder medFordeling(Fordeling fordeling) {
+        endringssoeknadKladd.setFordeling(fordeling);
+        return this;
+    }
+    public EndringssøknadBuilder medSaksnummer(String saksnummer) {
+        endringssoeknadKladd.setSaksnummer(saksnummer);
+        return this;
+    }
+
+    public Soeknad build() {
+        medYtelse(endringssoeknadKladd);
+        return super.build();
+    }
+
+
+}
