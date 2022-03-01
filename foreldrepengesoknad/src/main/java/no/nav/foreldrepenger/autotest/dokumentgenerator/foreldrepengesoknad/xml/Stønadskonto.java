4a62ef9c2eb298bd2e0a2b1015aa44a136593e78
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-03-01 12:59:29 +0100

    Gå over til JsonValue

--- foreldrepengesoknad/src/main/java/no/nav/foreldrepenger/autotest/dokumentgenerator/foreldrepengesoknad/xml/Stønadskonto.java
+++ foreldrepengesoknad/src/main/java/no/nav/foreldrepenger/autotest/dokumentgenerator/foreldrepengesoknad/xml/Stønadskonto.java
@@ -1,14 +1,8 @@
 package no.nav.foreldrepenger.autotest.dokumentgenerator.foreldrepengesoknad.xml;
 
-import java.util.Arrays;
-import java.util.Optional;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 
 @Deprecated
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum Stønadskonto {
 
     FELLESPERIODE,
@@ -17,32 +11,8 @@ public enum Stønadskonto {
     FORELDREPENGER,
     FLERBARNSDAGER,
     FORELDREPENGER_FØR_FØDSEL,
-    INGEN_STØNADSKONTO("-"),
+    @JsonEnumDefaultValue
+    INGEN_STØNADSKONTO,
     ;
 
-    private final String kode;
-
-    Stønadskonto() {
-        this(null);
-    }
-
-    Stønadskonto(String kode) {
-        this.kode = Optional.ofNullable(kode).orElse(name());
-    }
-
-    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-    public static Stønadskonto fraKode(@JsonProperty(value = "kode") Object node) {
-        if (node == null) {
-            return null;
-        }
-        var kode = TempAvledeKode.getVerdi(Stønadskonto.class, node, "kode");
-        return Arrays.stream(Stønadskonto.values())
-                .filter(value -> value.getKode().equalsIgnoreCase(kode))
-                .findFirst()
-                .orElse(Stønadskonto.INGEN_STØNADSKONTO);
-    }
-
-    public String getKode() {
-        return kode;
-    }
 }
